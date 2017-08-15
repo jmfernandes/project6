ca706ead1fc7f217865ed080f868d9c3b67e5b31
jmfernandes <joshfernandes@mac.com>
2017-08-15 16:51:44 -0700

    added start

--- js/controllers/StopwatchController.js
+++ js/controllers/StopwatchController.js
@@ -1,9 +1,13 @@
 mainApp.controller('StopwatchController', ['$scope', function($scope){
   $scope.message = 'This is the stopwatch app';
-  $scope.date = new Date();
   $scope.time_elapsed = 0;
 
+  $scope.get_date = function(){
+    return new Date();
+  };
+
   $scope.start = function(condition){
-    return condition;
+    $scope.initial_time = get_date();
+    return $scope.initial_time;
   };
 }]);
