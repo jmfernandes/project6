1ff1bf557b073e91933d820da45fb1c895411b95
jmfernandes <joshfernandes@mac.com>
2017-08-15 16:01:58 -0700

    changed links from prpject 5 to 6

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ var mainApp = angular.module("mainApp", ['ngRoute']);
 mainApp.config(function($routeProvider) {
 	$routeProvider
 		.when('/home', {
-			templateUrl: '/project5/assets/templates/home.html',
+			templateUrl: '/project6/assets/templates/home.html',
 			controller: 'HomeController'
 		})
 		.when('/stopwatch', {
-			templateUrl: '/project5/assets/templates/stopwatch.html',
+			templateUrl: '/project6/assets/templates/stopwatch.html',
 			controller: 'StopwatchController'
 		})
 		.otherwise({
