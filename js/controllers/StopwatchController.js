bdabe69738b39f017de4a570b9091ca3991836f1
jmfernandes <joshfernandes@mac.com>
2017-08-15 16:47:52 -0700

    added bind

--- js/controllers/StopwatchController.js
+++ js/controllers/StopwatchController.js
@@ -1,5 +1,9 @@
 mainApp.controller('StopwatchController', ['$scope', function($scope){
   $scope.message = 'This is the stopwatch app';
-  $scope.date1 = new Date();
-  $scope.date2 = new Date();
+  $scope.date = new Date();
+  $scope.time_elapsed = 0;
+
+  $scope.start = function(condition){
+    return condition;
+  };
 }]);
