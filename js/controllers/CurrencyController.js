ad940aff7822b045fddbf1ea6d699b110c196e70
jmfernandes <joshfernandes@mac.com>
2017-08-16 14:32:44 -0700

    did some work on currency converter

--- js/controllers/CurrencyController.js
+++ js/controllers/CurrencyController.js
@@ -1,3 +1,41 @@
 mainApp.controller('CurrencyController', ['$scope', function($scope){
   $scope.message = 'This is the currency page';
+  $scope.input = 0;
+  $scope.output = 0;
+  $scope.options =
+  [
+  {
+    name: 'American Dollar',
+    value: 'American Dollar'
+  },
+  {
+    name: 'Euro',
+    value: 'Euro'
+  },
+  {
+    name: 'Pound Sterling',
+    value: 'Pound Sterling'
+  },
+  {
+    name: 'Australian Dollar',
+    value: 'Australian Dollar'
+  },
+  {
+    name: 'Swiss Franc',
+    value: 'Swiss Franc'
+  },
+  {
+    name: 'Franc',
+    value: 'Franc'
+  },
+  {
+    name: 'Yen',
+    value: 'Yen'
+  }
+  ];
+
+  $scope.convert = function(){
+    $scope.output = 5
+  }
+
 }]);
