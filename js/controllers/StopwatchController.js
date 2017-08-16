fab25ccb0ede4d56e00ccbbecf48236bb5539494
jmfernandes <joshfernandes@mac.com>
2017-08-15 17:17:22 -0700

    Added stop

--- js/controllers/StopwatchController.js
+++ js/controllers/StopwatchController.js
@@ -2,20 +2,24 @@ mainApp.controller('StopwatchController', ['$scope', '$interval', function($scop
   $scope.message = 'This is the stopwatch app';
   $scope.time_elapsed = 0;
 
+  var repeat;
   $scope.get_date = function(){
     return new Date();
   };
 
   $scope.start = function(condition){
+    if (!repeat) {
     initial_time = $scope.get_date();
     repeat = $interval(function () {
       var now = new Date();
       $scope.time_elapsed = now.getTime() - initial_time.getTime();
     }, 10);
+  }
   };
 
   $scope.stop = function(){
     $interval.cancel(repeat);
+    repeat = undefined;
   };
 
 }]);
