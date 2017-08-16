418d1e2385029395b5fd6c09196bbfb0ee87c065
jmfernandes <joshfernandes@mac.com>
2017-08-15 18:05:12 -0700

    try to remove reset

--- js/controllers/StopwatchController.js
+++ js/controllers/StopwatchController.js
@@ -8,13 +8,13 @@ mainApp.controller('StopwatchController', ['$scope', '$interval', function($scop
   };
 
   $scope.start = function(){
-    if (!repeat) {
+    if ( angular.isUndefined(repeat) ){
     initial_time = $scope.get_date();
+  }
     repeat = $interval(function () {
       var now = new Date();
       $scope.time_elapsed = now.getTime() - initial_time.getTime();
     }, 10);
-  }
   };
 
   $scope.stop = function(){
@@ -30,33 +30,3 @@ mainApp.controller('StopwatchController', ['$scope', '$interval', function($scop
  });
 
 }]);
-
-mainApp.filter('millSecondsToTimeString', function() {
-  return function(millseconds) {
-    var oneSecond = 1000;
-    var oneMinute = oneSecond * 60;
-    var oneHour = oneMinute * 60;
-    var oneDay = oneHour * 24;
-
-    var seconds = Math.floor((millseconds % oneMinute) / oneSecond);
-    var minutes = Math.floor((millseconds % oneHour) / oneMinute);
-    var hours = Math.floor((millseconds % oneDay) / oneHour);
-    var days = Math.floor(millseconds / oneDay);
-
-    var timeString = '';
-    if (days !== 0) {
-        timeString += (days !== 1) ? (days + ' days ') : (days + ' day ');
-    }
-    if (hours !== 0) {
-        timeString += (hours !== 1) ? (hours + ' hours ') : (hours + ' hour ');
-    }
-    if (minutes !== 0) {
-        timeString += (minutes !== 1) ? (minutes + ' minutes ') : (minutes + ' minute ');
-    }
-    if (seconds !== 0 || millseconds < 1000) {
-        timeString += (seconds !== 1) ? (seconds + ' seconds ') : (seconds + ' second ');
-    }
-
-    return timeString;
-};
-});
